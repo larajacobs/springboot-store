89d9249dd980f4858b0d23efddc65154bb1021fa
Lara <mslarajacobs@gmail.com>
2025-09-12 19:23:30 +0200

    Basic spring boot project created

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # springboot-store
 Learning spring with mosh, creating a basic springboot application
+
+# Basic Springboot project
+## Running the project
+```
+    mvn spring-boot:run
+```
---
+++ store/src/test/java/com/practicespringboot/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicespringboot.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
